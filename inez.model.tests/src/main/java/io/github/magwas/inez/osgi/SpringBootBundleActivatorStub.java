7efa8c911d9c13994bac359e1bedfe6e2d628312
Arpad Magosanyi <m4gw4s@gmail.com>
2025-10-14 12:09:22 +0200

    got rid of tycho, restructured
    also deleted the ui. nothing of value have been lost :)

--- inez.model.tests/src/main/java/io/github/magwas/inez/osgi/SpringBootBundleActivatorStub.java
+++
@@ -1,9 +0,0 @@
-package io.github.magwas.inez.osgi;
-
-import static org.mockito.Mockito.mock;
-
-public class SpringBootBundleActivatorStub {
-	public static SpringBootBundleActivator stub() {
-		return mock(SpringBootBundleActivator.class);
-	}
-}
--- inez.ui/src/io/github/magwas/inez/ui/mindmap/visuals/ArrowHead.java
+++
@@ -1,9 +0,0 @@
-package io.github.magwas.inez.ui.mindmap.visuals;
-
-import javafx.scene.shape.Polygon;
-
-public class ArrowHead extends Polygon {
-	public ArrowHead() {
-		super(0, 0, 10, 3, 10, -3);
-	}
-}
